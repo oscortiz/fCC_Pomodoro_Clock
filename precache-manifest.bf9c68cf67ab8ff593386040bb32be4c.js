89cccfe9457ce9ecb1482f828983845ed61fc233
Óscar Ortiz <oscortiz@gmail.com>
2020-04-10 20:30:04 +0200

    Updates

---
+++ precache-manifest.bf9c68cf67ab8ff593386040bb32be4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c35eaa702d23eb64f87",
+    "url": "/fCC_Pomodoro_Clock/static/css/main.028fef99.chunk.css"
+  },
+  {
+    "revision": "1c35eaa702d23eb64f87",
+    "url": "/fCC_Pomodoro_Clock/static/js/main.1c35eaa7.chunk.js"
+  },
+  {
+    "revision": "59e11fabd551896501b4",
+    "url": "/fCC_Pomodoro_Clock/static/js/1.59e11fab.chunk.js"
+  },
+  {
+    "revision": "c563d00cdc5a1a22ae87",
+    "url": "/fCC_Pomodoro_Clock/static/js/runtime~main.c563d00c.js"
+  },
+  {
+    "revision": "c2cff6369defff6ac2df53eb6f0c1985",
+    "url": "/fCC_Pomodoro_Clock/index.html"
+  }
+];
